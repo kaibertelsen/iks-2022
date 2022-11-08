0f879e4a6277d834632060ed9c19cfcbce68f130
kaibertelsen <117456051+kaibertelsen@users.noreply.github.com>
2022-11-08 15:43:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+
+function lastinntextinput(){
+$('#resultat').val("{{wf {&quot;path&quot;:&quot;name&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}");
+$('#sum').val("{{wf {&quot;path&quot;:&quot;slug&quot;,&quot;type&quot;:&quot;PlainText&quot;\} }}");
+console.log("jepp");
+}
+
