761913e5ec595f5622e7ea46e214f14f5d01d052
kaibertelsen <117456051+kaibertelsen@users.noreply.github.com>
2022-12-15 14:33:31 +0100

    Update and rename main-05.js to main-06.js

--- main-06.js
+++ main-06.js
@@ -1,3 +1,30 @@
+function isdateoff(inndato,daysoff){
+// innkommende dato cms
+var splitdato = inndato.split('/');
+var inday = splitdato[0];
+var indmnd = splitdato[1];
+var inyear = splitdato[2];
+
+//dagens dato
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var yyyy = today.getFullYear();
+
+
+var valuetoday = ((Number (yyyy))*365)+((Number (mm))*30)+(Number (dd));
+var valuecreatday = ((Number (inyear))*365)+((Number (indmnd))*30)+(Number (inday));
+var sumdiff = (Number (valuetoday))-(Number (valuecreatday));
+
+if (sumdiff>daysoff){
+  //eldre enn 30 dager
+return false;
+}else{
+  //merkes med ny
+return true;
+}
+}
+
 function setselector(val,elementid){
 //brukes for å sette standard verdier
 // val = value to be set in the selector
